276a897e02badd86d12b74978982465b2cb94515
MrLukosius <lukasgaldikas1@gmail.com>
2025-02-19 22:06:08 +0200

    labas.py edit

--- commands/labas.py
+++ commands/labas.py
@@ -5,9 +5,17 @@ class Labas(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
-    async def labas(self, ctx):
-        await ctx.send("Labas! :)")
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if message.author.bot:
+            return  # Ignoruojame botų žinutes
+
+        lower_message = message.content.lower()  # Mažosios raidės, kad būtų jautrus tikrinimas
+
+        if lower_message == "labas":
+            await message.channel.send(f"Labas!, {message.author.mention}!")
+        elif lower_message in ["sveikas", "sveiki"]:
+            await message.channel.send(f"Sveikas/-a!, {message.author.mention}!")
 
 async def setup(bot):
     await bot.add_cog(Labas(bot))
